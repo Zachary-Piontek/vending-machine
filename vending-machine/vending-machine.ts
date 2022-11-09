bdcc486a58a65e414d4ddf116c7bd9fd6b742fcb
Zachary Piontek <zakpiontek@gmail.com>
2022-11-09 15:35:45 -0800

    added comments and fixed logic

--- vending-machine/vending-machine.ts
+++ vending-machine/vending-machine.ts
@@ -31,24 +31,27 @@ if(payment == null) {
 }
 
 const getChange = function(payment: number, cost: number) {
+    // calculate change
     let change = (payment - cost);
     console.log(`Your total change amount is ${change} cents.`);
+    // run through change amount and return correct change amount in currency
     if (change > 24) {
         console.log(`You recieve ${Math.floor(change/25)} quarters,`);
-        change = change - 25;
+        change = change % 25;
     }
     if (change > 9) {
         console.log(`${Math.floor(change/10)} dimes,`);
-        change = change - 10;
+        change = change % 10;
     }
     if (change > 4) {
         console.log(`${Math.floor(change/5)} nickels,`);
-        change = change - 5;
+        change = change % 5;
+    }
     if (change > 0) {
         console.log(`and ${Math.floor(change/1)} pennies back for your change.`);
-        change = change - 1;
+        change = change % 1;
     }
     }
-}
 
+// call function with params
 getChange(payment, cost);
